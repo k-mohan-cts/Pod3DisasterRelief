864e98c7d49141671066aac9d156419cad4f232a
Mohan <2478144@cognizant.com>
2026-03-05 13:32:29 +0530

    Initial File Structure of Disaster Management

---
+++ src/main/java/org/cognizant/disastermanagement/DisasterManagementApplication.java
@@ -0,0 +1,13 @@
+package org.cognizant.disastermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisasterManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cognizant/disastermanagement/DisasterManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cognizant.disastermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
