50d2a6fa3dc13dd9f5af4fd3c418c6f137649979
Kancharlaravisankar <KancharlaRaviSankar.Guptha@cognizant.com>
2026-03-16 10:06:42 +0530

    added

--- src/main/java/org/cognizant/disastermanagement/service/RecoveryService.java
+++ src/main/java/org/cognizant/disastermanagement/service/RecoveryService.java
@@ -1,10 +1,41 @@
 package org.cognizant.disastermanagement.service;
+
 import org.cognizant.disastermanagement.entity.RecoveryProgram;
+import org.cognizant.disastermanagement.Enum.RecoveryStatus;
+import org.cognizant.disastermanagement.dao.RecoveryProgramRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import java.util.List;
-public interface RecoveryService {
-    RecoveryProgram createProgram(RecoveryProgram program);
-    List<RecoveryProgram> getAllPrograms();
-    RecoveryProgram getProgramById(int id);
-    void cancelProgram(int id);
-    void deleteProgram(int id);
+
+@Service
+public class RecoveryService {
+
+    @Autowired
+    private RecoveryProgramRepository programRepository;
+
+    public RecoveryProgram createProgram(RecoveryProgram program) {
+        return programRepository.save(program);
+    }
+
+    public List<RecoveryProgram> getAllPrograms() {
+        return programRepository.findAll();
+    }
+
+    public RecoveryProgram getProgramById(int id) {
+        return programRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Program not found with id: " + id));
+    }
+
+    public void cancelProgram(int id) {
+        RecoveryProgram program = getProgramById(id);
+        program.setStatus(RecoveryStatus.CANCELLED);
+        programRepository.save(program);
+    }
+
+    public void deleteProgram(int id) {
+        if (!programRepository.existsById(id)) {
+            throw new RuntimeException("Cannot delete. Program not found.");
+        }
+        programRepository.deleteById(id);
+    }
 }
--- src/main/java/org/cognizant/disastermanagement/service/ResourceService.java
+++ src/main/java/org/cognizant/disastermanagement/service/ResourceService.java
@@ -1,13 +1,50 @@
 package org.cognizant.disastermanagement.service;
 
+import org.cognizant.disastermanagement.entity.RecoveryProgram;
 import org.cognizant.disastermanagement.entity.Resource;
 import org.cognizant.disastermanagement.Enum.ResourceStatus;
+import org.cognizant.disastermanagement.dao.RecoveryProgramRepository;
+import org.cognizant.disastermanagement.dao.ResourceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
-public interface ResourceService {
-    Resource addResource(int programId, Resource resource);
-    List<Resource> getAllResources();
-    List<Resource> getResourcesByProgram(int programId);
-    Resource updateResourceStatus(int resourceId, ResourceStatus status);
-    void deleteResource(int resourceId);
+@Service
+public class ResourceService {
+
+    @Autowired
+    private ResourceRepository resourceRepo;
+
+    @Autowired
+    private RecoveryProgramRepository programRepo;
+
+    @Transactional
+    public Resource addResource(int programId, Resource resource) {
+        RecoveryProgram program = programRepo.findById(programId)
+                .orElseThrow(() -> new RuntimeException("Program not found: " + programId));
+        resource.setRecoveryProgram(program);
+        return resourceRepo.save(resource);
+    }
+
+    public List<Resource> getAllResources() {
+        return resourceRepo.findAll();
+    }
+
+    public List<Resource> getResourcesByProgram(int programId) {
+        return resourceRepo.findByRecoveryProgramProgramId(programId);
+    }
+
+    @Transactional
+    public Resource updateResourceStatus(int resourceId, ResourceStatus status) {
+        Resource resource = resourceRepo.findById(resourceId)
+                .orElseThrow(() -> new RuntimeException("Resource not found: " + resourceId));
+        resource.setStatus(status);
+        return resourceRepo.save(resource);
+    }
+
+    @Transactional
+    public void deleteResource(int resourceId) {
+        resourceRepo.deleteById(resourceId);
+    }
 }
